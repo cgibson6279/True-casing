9a8c55432a48dbe5a7895127e97481105d87368a
Cameron Gibson <cgibson6279@Camerons-MBP.lan>
2021-04-15 18:47:16 -0400

    minor revisions to processing.sh

--- src/data/processing.sh
+++ src/data/processing.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Execute entire data processing pipeline
-# Run in folder that contains data
+# Run in directory containing data
 
 set -eou pipefail
 
 FILE=$1
 main() {
-    ./code/data_prep.py FILE train.tok dev.tok test.tok
+    ./code/data_prep.py $FILE train.tok dev.tok test.tok
     ./code/get_features.py train.tok train.features
     ./code/get_features.py dev.tok dev.features
     ./code/get_test_features.py test.tok test.features
-    ./code/mixed_case.py FILE mc.json
+    ./code/mixed_case.py $FILE mc.json
 }
 
-main FILE
+main $FILE
