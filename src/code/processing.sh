6a078a8522c999dadd6ce08b33d359630d9e9780
cgibson6279 <cameron.gibson@maine.edu>
2021-04-15 01:06:40 -0400

    full processing

--- src/code/processing.sh
+++ src/code/processing.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-./data_prep.py 
+"""processing.sh
 
+Execute entire data processing pipeline
+"""
 set -eou pipefail
-read data 
+
+read data
+
 main() {
-    ./data_prep.py "${data}" train.tok dev.tok} test.tok
+    ./data_prep.py $data train.tok dev.tok test.tok
     ./get_features.py train.tok train.features
     ./get_features.py dev.tok dev.features
     ./get_features.py test.tok test.features
-    ./mixed_case "${data}" mc.json
+    ./mixed_case $data mc.json
 }
 
 main
