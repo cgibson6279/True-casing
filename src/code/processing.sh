11a1b7afb326e38aba9813cce8598ba37ad3a1da
cgibson6279 <cameron.gibson@maine.edu>
2021-04-15 01:46:56 -0400

    full process

--- src/code/processing.sh
+++ src/code/processing.sh
@@ -4,14 +4,12 @@
 
 set -eou pipefail
 
-read data
-
 main() {
-    ./data_prep.py "${data}" train.tok dev.tok test.tok
+    ./data_prep.py news.2007.gz train.tok dev.tok test.tok
     ./get_features.py train.tok train.features
     ./get_features.py dev.tok dev.features
     ./get_features.py test.tok test.features
-    ./mixed_case "${data}" mc.json
+    ./mixed_case news.2007.gz mc.json
 }
 
 main
